e9b0b810b3265c601e18c62d38e52638a15e64ee
Monkshoody <stefan-wuest@outlook.de>
2026-03-05 10:12:08 +0100

    try to find assets

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const REVERSE_DOMAIN = "eu.armindo";
 export const APP_KEY = `${REVERSE_DOMAIN}.embers`;
-export const ASSET_LOCATION = import.meta.env.VITE_ASSET_LOCATION ?? `${window.location.origin}/Library`;
+export const ASSET_LOCATION = import.meta.env.VITE_ASSET_LOCATION ?? `https://jb2a-free.s3.eu-west-3.amazonaws.com/Library`;
