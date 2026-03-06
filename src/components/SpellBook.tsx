adcf65d8d0b95346da33d0afd986d2b73ba65eb0
Monkshoody <stefan-wuest@outlook.de>
2026-03-06 13:33:22 +0100

    messge is send. So the Listener is the problem. Cleaning up.

--- src/components/SpellBook.tsx
+++ src/components/SpellBook.tsx
@@ -356,11 +356,9 @@ export default function SpellBook() {
                             className="clickable"
                             size="small"
                             onClick={async () => {
-                                console.log("Sending sync request");
                                 await OBR.broadcast.sendMessage("magician/sync-request", {
                                     playerId: OBR.player.id
                                 });
-                                console.log("Message sent");
                                 OBR.notification.show("Sync request sent to Character Sheet", "INFO");
                             }}
                         >
